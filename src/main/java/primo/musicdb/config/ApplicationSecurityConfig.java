efd0baae13d85be859b8d0dab9753b0bfc8ef5a4
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-08-04 11:53:10 +0300

    Rename img to images

--- src/main/java/primo/musicdb/config/ApplicationSecurityConfig.java
+++ src/main/java/primo/musicdb/config/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package primo.musicdb.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,7 +24,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/js/**", "/css/**", "/img/**").permitAll()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .antMatchers("/", "/users/login", "/users/register").permitAll()
                 .antMatchers("/**").authenticated()
                 .and()
